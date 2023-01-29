dff39fd48071fdf56f0218aa598b1b0ba2f9720f
현원 <jhwjhwjhw97@gmail.com>
2023-01-30 00:00:22 +0900

    done

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "f233c94b0cf28afd2a4c70d211dd0bc1";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("You live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
